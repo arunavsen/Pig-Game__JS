ca3337b4729a224cb5bd60f6ff9ddf0dae30394a
Arunav <arunav@vonome.com>
2021-01-09 13:18:05 +0600

    Implementing our HOLD function and the DRY principle

--- app-clear.js
+++ app-clear.js
@@ -1,5 +1,6 @@
 var roundScore = 0;
 var activePlayer = 0;
+var scores = [0, 0]
 
 document.getElementById('score-0').textContent = '0'
 document.getElementById('score-1').textContent = '0'
@@ -10,33 +11,55 @@ document.getElementById('current-1').textContent = '0'
 
 document.querySelector('.dice-1').style.display = 'none';
 
-document.querySelector('.btn-roll').addEventListener('click',function(){
+document.querySelector('.btn-roll').addEventListener('click', function () {
     // 1. Random number
-    var dice = Math.floor(Math.random()*6)+1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display the result
     var diceDOM = document.querySelector('.dice-1');
     diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-'+dice+'.png';
+    diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. Update the round score If the rolled number was NOT a 1
-    if(dice !== 1){
+    if (dice !== 1) {
         // Add Score
         roundScore += dice;
-        document.querySelector('#current-'+ activePlayer).textContent=roundScore;
-    }else{
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
         //Next player
-        activePlayer === 0?activePlayer=1:activePlayer=0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice-1').style.display = 'none';
-
+        newPlayer();
     }
-
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // 1. Add Current Score to Global Score
+    scores[activePlayer] += roundScore;
+
+    // 2. Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // 3. Check if the player won the game
+    if (scores[activePlayer] >= 50) {
+        document.querySelector('#name-' + activePlayer).textContent = 'WINNER !';
+        document.querySelector('.dice-1').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+
+    }else{
+// 4. New Player
+newPlayer();
+    }
+});
+
+function newPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice-1').style.display = 'none';
+}
