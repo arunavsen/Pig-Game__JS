aac4ee5f3adb82386ff2af9b7937ec576ee57c77
Arunav <arunav@vonome.com>
2021-01-11 10:24:13 +0600

    Finishing touches state variables

--- app-clear.js
+++ app-clear.js
@@ -1,54 +1,59 @@
-var roundScore, activePlayer, scores;
+var roundScore, activePlayer, scores, gamePlaying;
 
 // Calling Init function to initialize the game
 init();
 
 // Roll Event
 document.querySelector('.btn-roll').addEventListener('click', function () {
-    // 1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    // If gamePlaying is true, we allow player to play the game, otherwise not.
+    if (gamePlaying) {
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    // 2. Display the result
-    var diceDOM = document.querySelector('.dice-1');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+        // 2. Display the result
+        var diceDOM = document.querySelector('.dice-1');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
-    // 3. Update the round score If the rolled number was NOT a 1
-    if (dice !== 1) {
-        // Add Score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        newPlayer();
+        // 3. Update the round score If the rolled number was NOT a 1
+        if (dice !== 1) {
+            // Add Score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            newPlayer();
+        }
     }
+
 });
 
 // Hold event
 document.querySelector('.btn-hold').addEventListener('click', function () {
+    if (gamePlaying) {
+        // 1. Add Current Score to Global Score
+        scores[activePlayer] += roundScore;
 
-    // 1. Add Current Score to Global Score
-    scores[activePlayer] += roundScore;
+        // 2. Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    // 2. Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-    // 3. Check if the player won the game
-    if (scores[activePlayer] >= 50) {
-        document.querySelector('#name-' + activePlayer).textContent = 'WINNER !';
-        document.querySelector('.dice-1').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-
-    } else {
-        // 4. New Player
-        newPlayer();
+        // 3. Check if the player won the game
+        if (scores[activePlayer] >= 50) {
+            document.querySelector('#name-' + activePlayer).textContent = 'WINNER !';
+            document.querySelector('.dice-1').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            // 4. New Player
+            newPlayer();
+        }
     }
 });
 
 
 // New Game Event
-document.querySelector('.btn-new').addEventListener('click',init);
+document.querySelector('.btn-new').addEventListener('click', init);
 
 function newPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -69,6 +74,7 @@ function init() {
     roundScore = 0;
     activePlayer = 0;
     scores = [0, 0]
+    gamePlaying = true;
 
     document.querySelector('.dice-1').style.display = 'none';
 
@@ -81,5 +87,5 @@ function init() {
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.add('active');
-    
+
 }
